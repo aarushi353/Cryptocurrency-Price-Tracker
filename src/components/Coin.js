5ba517e0ac3261c33557cd74153242e3a10f693c
Aarushi <72608824+aarushi353@users.noreply.github.com>
2022-10-09 11:00:45 +0530

    deploy

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,23 +1,48 @@
 import React from "react";
-import "./Coins.module.css";
- const Coin = ({name, price, symbol, marketcap, volume, image, priceChange}) => {
+import "./Coins.css";
+const Coin = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+}) => {
   return (
     <div className="coin">
-      <img
-        src={image}
-        className="coin-logo"
-      />
+      <center>
+        <img src={image} className="coin-logo" />
+      </center>
       <div className="coin-content">
-        <h1 className="coin-title">{name}</h1>
-        <p className="coin-symbol">{symbol}</p>
+        <center>
+          <h1 className="coin-title">{name}</h1>
+        </center>
+        <center>
+          <p className="coin-symbol">{symbol}</p>
+        </center>
       </div>
-      <p className="coin-price">{price.toLocaleString()}</p>
-      <p className="coin-marketcap">
+      <center>
+        {" "}
+        <p className="coin-price">{price.toLocaleString()}</p>
+        <p className="coin-marketcap">
           Market Cap: ${marketcap.toLocaleString()}
-      </p>
-      <p className="coin-volume">
-      Volume (24Hrs): ${volume.toLocaleString()}
-      </p>
+        </p>
+        <p className="coin-volume">
+          Volume (24Hrs): ${volume.toLocaleString()}
+        </p>
+      </center>
+      {priceChange < 0 ? (
+        <div className="priceContainerDown">
+          <i className="fas fa-arrow-down"></i>
+          <p className="priceChange">{priceChange.toFixed(2)}%</p>
+        </div>
+      ) : (
+        <div className="priceContainerUp">
+          <i className="fas fa-arrow-up"></i>
+          <p className="priceChange">{priceChange.toFixed(2)}%</p>
+        </div>
+      )}
     </div>
   );
 };
